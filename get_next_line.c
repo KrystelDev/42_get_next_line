ded1d2bcee2e4d22eeeea6cb3467ea9952c4025e
Krystel Rodriguez Castejon <kryrodri@car4s5.42barcelona.com>
2023-10-04 10:58:16 +0200

    arreglando algunos leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kryrodri <kryrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 11:45:07 by kryrodri          #+#    #+#             */
-/*   Updated: 2023/10/01 15:53:29 by kryrodri         ###   ########.fr       */
+/*   Updated: 2023/10/01 17:51:14 by kryrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,37 +51,44 @@ char *get_next_line(int fd)
 {
     static char *reading = NULL; // que ha leido read (quizá deberia inicializar en otra función).
     int q_chars; // que devuelve read (quizá deberia inicializar en otra función).
-    // int i_line; // para localizar el salto de linea. (quizá deberia inicializar en otra función).
     char *result;
+    char *sub_string;
     
     result = NULL; // lo que devuelve la función principal.
 
 // Controlo el error de open.
-    if (fd < 0 || BUFFER_SIZE <= 0 || (read(fd, 0,0)))
+    if (fd < 0 || BUFFER_SIZE <= 0 || (read(fd, 0, 0)))
         return (NULL);
 
 // 1- Hay algo pendiente a devolver ? >> while hasta que sea Sí o Nada que leer
     while (!reading) //    while (!reading || !q_chars)
-    {
-        reading = malloc(BUFFER_SIZE + 1);
-        if(!reading)
         {
-            // if (result)
-            // {
-            //     ft_clean(&result);
-            // }
-            return (NULL);
+            reading = malloc(BUFFER_SIZE + 1);
+            if(!reading)
+                return (NULL);
+        // 1-2 No pendiente ->
+                // Lee
+            q_chars = read(fd, reading, BUFFER_SIZE);
+            if (q_chars == -1) 
+            {
+                // -1 si hay algun error de reading
+                return NULL;
+            }
+            if (q_chars == 0 && result) // Sí -> Guarda en result desde el inicio de pendiente hasta el final.
+            {
+                ft_clean(&reading);
+                if(result && result[0] == '\0')
+                    return(NULL);
+                return (result);
+            }
+            reading[q_chars] = '\0';
         }
-    // 1-2 No pendiente ->
-            // Lee
-        q_chars = read(fd, reading, BUFFER_SIZE);
-        reading[q_chars] = '\0';
-    }
 
     // Si pongo esto salta mas errores.. T-T
-    // if (q_chars <= 0) 
+    // if (q_chars < 0) 
     // {
     //     // -1 si hay algun error de reading
+    //     ft_clean(&reading);
     //     return (NULL);
     // }
 
@@ -112,7 +119,7 @@ char *get_next_line(int fd)
                 {
                     ft_clean(&reading);
                     if(result && result[0] == '\0')
-                        return(NULL);
+                        ft_clean(&result);
                     return (result);
                 }
                 reading[q_chars] = '\0';
@@ -121,17 +128,27 @@ char *get_next_line(int fd)
         // Sí -> Guarda en result desde el inicio de pendiente hasta el '\n'
     if (ft_look_for_line(reading) != -1)
     {
-        result = ft_strjoin(result, ft_substr(reading, 0, ft_look_for_line(reading)+1));
+        sub_string = ft_substr(reading, 0, ft_look_for_line(reading)+1);
+        if (!sub_string)
+        {
+            ft_clean(&result);
+        }
+        else
+        {
+            result = ft_strjoin(result, sub_string);
+            ft_clean(&sub_string);
+
+        }
         if (!result)
         {
+            // ft_clean(&reading); //no soluciona nada
             return (NULL);
         }
         reading = ft_substr(reading, ft_look_for_line(reading)+1, ft_strlen(reading)-1);
-      
     }
     if (q_chars == 0)
         ft_clean(&reading);
     if(result && result[0] == '\0')
-        return(NULL);
+        ft_clean(&result);
     return (result);
 }
